43f06dc0b9a0b001708e9a61adaa5cec90eb1070
ReehcQ <mingqian.zhang@outlook.com>
2019-01-14 19:35:51 -0500

    add comment

--- dogAPI.py
+++ dogAPI.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Author: MingZZZZZZZZ
-# @Date created:
-# @Date last modified:
+# @Date created: 11 Jan 2019
+# @Date last modified: 14 Jan 2019
 # Python Version: Python3.6
 # Description:
 
@@ -13,14 +13,14 @@ import requests
 import json
 from collections import defaultdict
 
-# 初始化设置google fusion api
+# the setting of google fusion tables api
 api_key = "AIzaSyDOVEKSUYZOIq-_evlnxPOtqFe2Z_N2gy4"
 table_id = "1pKcxc8kzJbBVzLu_kgzoAMzqYhZyUhtScXjB0BQ"
 url_ft = "https://www.googleapis.com/fusiontables/v2/tables/%s?key=%s" % (table_id, api_key)
-columns = list(map(lambda x:x['name'].lower(), requests.get(url_ft).json()['columns'][:])) # 获取表的所有列名
+columns = list(map(lambda x:x['name'].lower(), requests.get(url_ft).json()['columns'][:])) # retrieve the name of columns in the table
 
 
-#定义服务port
+# define the sever port using 5000
 tornado.options.define('port', type=int, default=5000, help="server port")
 
 class CountHandler(tornado.web.RequestHandler):
@@ -29,27 +29,34 @@ class CountHandler(tornado.web.RequestHandler):
     def set_default_headers(self):
         self.set_header("Content-Type","application/json")
 
+    def write_error(self, scode, **kwargs):
+        if scode == 400:
+            self.write(str(kwargs['content']))
+
     def get(self):
         global table_id, api_key
-        # 提取参数
+        # retrieve the arguments
         args = self.request.arguments
-        # 判断参数是否有效, 如果全都无效，返回unknown field并打出400信号
-        # 如果只有部分无效则无视
+        # initialize the list of unknown keywords and the dictionary of params
         unknown_fields = []
-        kw_dict = defaultdict(str) #初始化装载参数的字典
+        kw_dict = defaultdict(str)
+        # classify the arguments
+        # since it is case insensitive, the args are transferred in lower case
         for arg_temp in args:
             arg = arg_temp.lower()
             if arg in columns:
                 kw_dict[arg] = self.get_arguments(arg_temp)[0].lower()
             else:
                 unknown_fields.append(arg_temp)
+        # if any invalid query string parameters are provided, return an error message containing the unknown fields
         if unknown_fields:
-            self.set_status(400)
-            self.write(json.dumps({'unknown fields': sorted(unknown_fields)}))
+            self.send_error(400, content=json.dumps({'unknown fields': sorted(unknown_fields)}))
         else:
-            # 拼接参数为query函数并从fusion table获取数据
-            # fusion table大小写敏感，并且不支持lower或者upper函数
-            # 分全小写，全大写和单词首字母大写的情况
+            # the query for the data in fusion tables(FT) is case sensitive, but FT does not support lower or upper function
+            # 3 cases are considered here:
+            # Upper case
+            # Lower case
+            # Title case
             query = ''  
             for arg in kw_dict:
                 if query:
@@ -59,11 +66,12 @@ class CountHandler(tornado.web.RequestHandler):
                         + (str(kw_dict[arg])).lower() + "','"\
                         + (str(kw_dict[arg])).title() + "'"\
                         + ")"
+            # query in the Google fusion tables API
             url_request = "https://www.googleapis.com/fusiontables/v2/query?sql=select count(dog_name) FROM %s where %s&key=%s" % (table_id, query, api_key)
             count_data = {'count': -999}
             try:
                 count_data['count'] = int(requests.get(url_request).json()['rows'][0][0])
-            except:
+            except KeyError: # if there is not any row in the dataset
                 count_data['count'] = 0
             count_json = json.dumps(count_data)
             self.write(count_json)
